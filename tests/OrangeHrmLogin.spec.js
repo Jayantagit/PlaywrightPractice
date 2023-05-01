5f33239d72d67e99af8d8a5385206cf3624f588c
Jayantagit <jayantamandal2006@gmail.com>
2023-05-01 18:37:15 +0530

    First Commit

---
+++ tests/OrangeHrmLogin.spec.js
@@ -0,0 +1,13 @@
+const {test,expect}=require("@playwright/test");
+
+test('Login Test', async({page}) => {
+
+   await  page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+   await  page.getByPlaceholder("Username").type("Admin");
+   await  page.getByPlaceholder("Password").type("admin123");
+   await page.locator("//button[@type='submit']").click();
+   await expect(page.getByText("Dashboard")).toBeVisible
+
+
+    
+});
